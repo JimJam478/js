98bd7d7d43b25387258d338494ef88957ca0a0ac
JimJam47 <allensreebinoj2010@gmail.com>
2024-01-08 16:35:16 +0530

    Resets count after hitting save

--- people_counter/index.js
+++ people_counter/index.js
@@ -10,5 +10,7 @@ function increment() {
 function save() {
     let entries = count + " - "
     saveEl.textContent += entries
+    count = 0
+    document.getElementById("count-el").textContent = count
     console.log(count)
 }
