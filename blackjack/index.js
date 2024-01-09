482dedbad92961d07398db43d621896566bd79ea
Allensree Binoj K <allen@hamon.in>
2024-01-09 12:59:33 +0530

    plays blackjack game

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,14 +1,33 @@
 let firstCard = 11
-let secondCard = 5
+let secondCard = 10
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 sum = firstCard + secondCard
+console.log(sum)
 
-if (sum < 21) {
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+
+function startGame() {
+    if (sum < 21) {
+    message = "Try again"
     console.log("Try again")
-}
-else if (sum === 21) {
+    }
+    else if (sum === 21) {
+    message = "Blackjack"
     console.log("Blackjack")
-}
-else {
+    hasBlackJack = true
+    }
+    else {
+    message = "You Lost !"
     console.log("You lost")
+    isAlive = false
+        }
+    messageEl.textContent = message
+    cardEl.textContent = "Card:" + " "+ firstCard + " " + "," + " " + secondCard
+    sumEl.textContent = "Sum:" + " " + sum
 }
+
