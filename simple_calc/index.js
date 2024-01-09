7d58cbc84b616180e382587911ff587c2c7f0ba9
Allensree Binoj K <allen@hamon.in>
2024-01-09 13:00:04 +0530

    adds let before assigning variable

--- simple_calc/index.js
+++ simple_calc/index.js
@@ -1,24 +1,24 @@
-let num1 = 5
-let num2 = 10
+var num1 = 10
+var num2 = 5
 document.getElementById("num1-el").textContent = num1 
 document.getElementById("num2-el").textContent = num2
 
 function sum() {
-    sum = num1 + num2
+    let sum = num1 + num2
     document.getElementById("result").textContent = sum
 }
 
 function sub() {
-    sub = num1 - num2
+    let sub = num1 - num2
     document.getElementById("result").textContent = sub
 }
 
 function mul() {
-    mul = num1 * num2
+    let mul = num1 * num2
     document.getElementById("result").textContent = mul
 }
 
 function div() {
-    div = num1 / num2
+    let div = num1 / num2
     document.getElementById("result").textContent = div
 }
